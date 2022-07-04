52bda8208fa767afee23e1dc6f4274b9490ef438
Thomas Remmer <remmert765@gmail.com>
2022-07-04 15:50:25 +0100

    made calculator fit to screen size

--- main.js
+++ main.js
@@ -5,63 +5,57 @@ let currentNum = document.getElementById("calculator__current")
 let prevNum = document.getElementById("calculator__previous")
 let operation = document.getElementById("calculator__opperator")
 
+numButton.forEach(input => input.addEventListener("click", (event) => {
 
+    if (event.target.innerText <= 9) {
+        currentNum.innerText += event.target.innerText;
+    }
+    else if (event.target.innerText == "AC") {
+        currentNum.innerText = "";
+        prevNum.innerText = "";
+        operation.innerText = ""
+    }
 
+    else if (event.target.id == "decimalButton") {
+        currentNum.innerText += event.target.innerText;
+    }
+    else if (event.target.id == "delButton") {
+        currentNum.innerText = currentNum.innerText.slice(0, -1)
+    }
+    else if (event.target.id == "equalButton") {
 
-
-
-
-    numButton.forEach (input => input.addEventListener("click", (event) => {
-
-        if (event.target.innerText <= 9 ) {
-            currentNum.innerText+= event.target.innerText;
+        switch (operation.innerText) {
+            case "+":
+                currentNum.innerText = Number(prevNum.innerText) + Number(currentNum.innerText);
+                prevNum.innerText = ""
+                break;
+            case "-":
+                currentNum.innerText = Number(prevNum.innerText) - Number(currentNum.innerText);
+                prevNum.innerText = ""
+                break;
+            case "*":
+                currentNum.innerText = Number(prevNum.innerText) * Number(currentNum.innerText);
+                prevNum.innerText = ""
+                break;
+            case "/":
+                currentNum.innerText = Number(prevNum.innerText) / Number(currentNum.innerText);
+                prevNum.innerText = ""
+                break;
         }
-        else if(event.target.innerText == "AC") {
-            currentNum.innerText ="";
-            prevNum.innerText ="";  
-            operation.innerText =""
-        }
-    
-        else if(event.target.id == "decimalButton") {
-            currentNum.innerText+= event.target.innerText;
-        }
-        else if(event.target.id == "delButton") {
-            currentNum.innerText = currentNum.innerText.slice(0,-1)
-        }
-        else if (event.target.id == "equalButton") {
+    }
+    else if (event.target.id == "plusButton" || "minusButton" || "divButton" || "timesButton") {
 
-            switch(operation.innerText) {
-                case "+":
-                    currentNum.innerText = Number(prevNum.innerText) + Number(currentNum.innerText);
-                    prevNum.innerText =""
-                    break;
-                case "-":
-                    currentNum.innerText = Number(prevNum.innerText) - Number(currentNum.innerText);
-                    prevNum.innerText =""
-                    break;
-                case "*":
-                    currentNum.innerText = Number(prevNum.innerText) * Number(currentNum.innerText);
-                    prevNum.innerText =""
-                    break;
-                case "/":
-                    currentNum.innerText = Number(prevNum.innerText) / Number(currentNum.innerText);
-                    prevNum.innerText =""
-                    break;
-            }
-        }
-        else if(event.target.id == "plusButton" || "minusButton" || "divButton" ||"timesButton") {
-            
-            prevNum.innerText = currentNum.innerText;
-            currentNum.innerText =""; 
-            operation.innerText = event.target.innerText
-            
-        }
-    
-    
-    
-        
-        }
-    ))
-    
+        prevNum.innerText = currentNum.innerText;
+        currentNum.innerText = "";
+        operation.innerText = event.target.innerText
+
+    }
+
+
+
+
+}
+))
+
 
 
