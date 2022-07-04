dff79997d896b2f6ebd92d1ca705258886323b49
Thomas Remmer <remmert765@gmail.com>
2022-07-04 14:21:46 +0100

    completed calculator logic

--- main.js
+++ main.js
@@ -3,41 +3,46 @@ let numButton = Array.from(document.querySelectorAll(".calculator__button"))
 //selects the bottom Div as the current number div
 let currentNum = document.getElementById("calculator__current")
 let prevNum = document.getElementById("calculator__previous")
+let operation = document.getElementById("calculator__opperator")
 
 
 
 
 numButton.forEach (input => input.addEventListener("click", (event) => {
-    // currentNum.innerHTML += event.target.innerHTML
-    // switch(event.target.innerText) {
-    //     case "AC":
-    //         currentNum.innerText ="";
-    //         break;
-    //     case "+":
-    //         prevNum.innerText = currentNum.innerText;
 
-    //     default:
-    //         currentNum.innerText+= event.target.innerText;
-
-    if (event.target.innerText <= 9) {
+    if (event.target.innerText <= 9 ) {
         currentNum.innerText+= event.target.innerText;
     }
     else if(event.target.innerText == "AC") {
         currentNum.innerText ="";
         prevNum.innerText ="";  
+        operation.innerText =""
     }
-    else if(event.target.innerText == "+" || "-" || "/" ||"*") {
+
+    else if(event.target.id == "decimalButton") {
+        currentNum.innerText+= event.target.innerText;
+    }
+    else if (event.target.id == "equalButton") {
+        let answer = currentNum.innerText + operation.innerText + prevNum.innerText
+        console.log(answer)
+        answer = eval(answer)
+        console.log(answer)
+        currentNum.innerText = answer
+        prevNum.innerText =""
+        operation.innerText = ""
+    }
+    else if(event.target.id == "plusButton" || "minusButton" || "divButton" ||"timesButton") {
         
         prevNum.innerText = currentNum.innerText;
         currentNum.innerText =""; 
+        operation.innerText = event.target.innerText
+        
     }
 
-    else if(event.target.innerText = "=") {
-        alert("you did it")
-    }
+
+
     
     }
-// }
 ))
 
 
