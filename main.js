1486fae61a70f8365301aa61f1647e390d681ca5
Thomas Remmer <remmert765@gmail.com>
2022-07-05 18:47:19 +0100

    moved logic to functions for improved clarity

--- main.js
+++ main.js
@@ -5,64 +5,83 @@ let operation = document.getElementById("calculator__opperator")
 numButton.forEach(input => input.addEventListener("click", (event) => {
 
     if (event.target.innerText <= 9) {
-        currentNum.innerText += event.target.innerText;
+        handleAddNum();
     }
-    else if (event.target.innerText == "AC") {
-        currentNum.innerText = "";
-        prevNum.innerText = "";
-        operation.innerText = ""
+    else if (event.target.id == "clearButton") {
+        handleClear();
     }
 
     else if (event.target.id == "decimalButton") {
-        
-        if (currentNum.innerText.includes(".")) {
-            alert("Current number already has one decimal")
-        }
-        else {
-            currentNum.innerText += event.target.innerText;
-        }
+        handleDecimal();
+
     }
     else if (event.target.id == "delButton") {
         currentNum.innerText = currentNum.innerText.slice(0, -1)
     }
     else if (event.target.id == "equalButton") {
+        handleEquals();
 
-        switch (operation.innerText) {
-            case "+":
-                currentNum.innerText = Number(prevNum.innerText) + Number(currentNum.innerText);
-                prevNum.innerText = ""
-                operation.innerText = ""
-                break;
-            case "-":
-                currentNum.innerText = Number(prevNum.innerText) - Number(currentNum.innerText);
-                prevNum.innerText = ""
-                operation.innerText = ""
-                break;
-            case "*":
-                currentNum.innerText = Number(prevNum.innerText) * Number(currentNum.innerText);
-                prevNum.innerText = ""
-                operation.innerText = ""
-                break;
-            case "/":
-                currentNum.innerText = Number(prevNum.innerText) / Number(currentNum.innerText);
-                prevNum.innerText = ""
-                operation.innerText = ""
-                break;
-        }
     }
     else if (event.target.id == "plusButton" || "minusButton" || "divButton" || "timesButton") {
-        switch (operation.innerText) {
-            case "":
-                prevNum.innerText = currentNum.innerText;
-                currentNum.innerText = "";
-                operation.innerText = event.target.innerText
-                break;
-            default:
-                operation.innerText = event.target.innerText
-                break;
-        }
-
+        handleOpperator();
 
     }
 }
 ))
+
+const handleOpperator = () => {
+    switch (operation.innerText) {
+        case "":
+            prevNum.innerText = currentNum.innerText;
+            currentNum.innerText = "";
+            operation.innerText = event.target.innerText
+            break;
+        default:
+            operation.innerText = event.target.innerText
+            break;
+    }
+
+}
+const handleEquals = () => {
+    switch (operation.innerText) {
+    case "+":
+        currentNum.innerText = Number(prevNum.innerText) + Number(currentNum.innerText);
+        prevNum.innerText = ""
+        operation.innerText = ""
+        break;
+    case "-":
+        currentNum.innerText = Number(prevNum.innerText) - Number(currentNum.innerText);
+        prevNum.innerText = ""
+        operation.innerText = ""
+        break;
+    case "*":
+        currentNum.innerText = Number(prevNum.innerText) * Number(currentNum.innerText);
+        prevNum.innerText = ""
+        operation.innerText = ""
+        break;
+    case "/":
+        currentNum.innerText = Number(prevNum.innerText) / Number(currentNum.innerText);
+        prevNum.innerText = ""
+        operation.innerText = ""
+        break;
+}
+}
+
+const handleDecimal = () => {
+    if (currentNum.innerText.includes(".")) {
+        alert("Current number already has one decimal")
+    }
+    else {
+        currentNum.innerText += event.target.innerText;
+    }
+}
+
+const handleClear = () => {
+    currentNum.innerText = "";
+    prevNum.innerText = "";
+    operation.innerText = ""
+}
+
+const handleAddNum = () => {
+    currentNum.innerText += event.target.innerText;
+}
